b62001dfe878b49ecd179c88f3b32425646c8361
Lanyi Knight <sinmaplewing@gmail.com>
2013-04-06 22:59:03 +0800

    Let -1 become CUTOFF.

--- AI2013-HW2-PancakesPuzzle-499470098.cpp
+++ AI2013-HW2-PancakesPuzzle-499470098.cpp
@@ -4,6 +4,7 @@
 
 const unsigned int SOLVE_NUM = 1000;
 const unsigned int MAX_DEPTH_SEARCH = 1000;
+const int CUTOFF = -1;
 
 int InputProcess( int initialPancake[], const char path[] );
 void SwapPancake( int pancake[], int position );
@@ -20,7 +21,7 @@ int main(){
 
   int pancakeLength = InputProcess(pancake, "input.txt");
   int move = IterativeDeepeningSearch( swapOrder, pancake, pancakeLength );
-  if( move != -1 ){
+  if( move != CUTOFF ){
     fprintf( fp, "An optimal solution has %d moves.\n", move );
     OutputPancake( fp, pancake, pancakeLength );
     for( int i = move-1 ; i >= 0 ; --i ){
@@ -64,14 +65,14 @@ int IterativeDeepeningSearch( int swapOrder[], int pancake[], int pancakeLength 
 
   for( int i = 0 ; i < MAX_DEPTH_SEARCH ; ++i ){
     int result = DepthLimitedSearch(swapOrder, pancake, pancakeLength, i, 0);
-    if( result != -1 ) return result;
+    if( result != CUTOFF ) return result;
   }
-  return -1;
+  return CUTOFF;
 }
 
 int DepthLimitedSearch( int swapOrder[], int pancake[], int pancakeLength, int depth, int lastSwap ){
   if( IsFinalState(pancake, pancakeLength) ) return depth;
-  if( depth == 0 ) return -1;
+  if( depth == 0 ) return CUTOFF;
 
   for( int i = 1 ; i < pancakeLength ; i++ ){
     if( i == lastSwap ) continue;
@@ -80,10 +81,10 @@ int DepthLimitedSearch( int swapOrder[], int pancake[], int pancakeLength, int d
 
     int result = DepthLimitedSearch( swapOrder, pancake, pancakeLength, depth-1, i);
     SwapPancake( pancake, i );
-    if( result != -1 ) return depth;
+    if( result != CUTOFF ) return depth;
     swapOrder[depth-1] = 0;
   }
-  return -1;
+  return CUTOFF;
 }
 
 bool IsFinalState( const int pancake[], int pancakeLength ){
